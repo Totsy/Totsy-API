7db61737cc161ddad3e159a7403fb151dfb3d580
Tharsan Bhuvanendran <tbhuvanendran@totsy.com>
2012-07-20 17:12:18 +0000

    Updated cache stats page to include the cache type.

--- cachestats.php
+++ cachestats.php
@@ -11,16 +11,23 @@ require_once 'app/autoload.php';
 define('API_ENV', getenv('API_ENV') ? getenv('API_ENV') : 'dev');
 
 // setup the local cache object by parsing the memcache configuration
+$stats = array('type' => 'none');
 $confMemcacheFile = 'etc/' . API_ENV . '/memcache.yaml';
-if (file_exists($confMemcacheFile)) {
+if (extension_loaded('yaml') && file_exists($confMemcacheFile)) {
     $confMemcache = yaml_parse_file($confMemcacheFile);
     $memcache = new Memcache();
-    $memcache->addServer($confMemcache['host'], $confMemcache['port']);
+    foreach ($confMemcache['servers'] as $server) {
+        $memcache->addServer($server['host'], $server['port']);
+    }
 
     $cache = new MemcacheCache();
     $cache->setMemcache($memcache);
+    $stats['type'] = 'memcache';
 } else {
     $cache = new ApcCache();
+    $stats['type'] = 'apc';
 }
 
-echo json_encode($cache->getStats());
+$stats = array_merge($stats, $cache->getStats());
+
+echo json_encode($stats);
